8d06e13f1287516849fa2631b868aaf51760033b
FalehHassan <313398088+FalehHassan@users.noreply.github.com>
2026-09-17 16:42:27 +0200

    Make human choice case-insensitive

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 
 // Function getComputerChoice
 
-/* This function will randomly return rock, paper or scissors  */
-
+// This function will randomly return rock, paper or scissors 
 
 function getComputerChoice() {
     const randomNum = Math.floor(Math.random()*3) + 1; 
@@ -26,16 +25,25 @@ const ComputerChoice = getComputerChoice();
 console.log(ComputerChoice);
 
 
-
+// This function will prompt the user for their input
 function getHumanChoice() {
 
     let userChoice;
-    
 
-    userChoice = prompt("rock, paper, scissors...");
+    userChoice = prompt("rock, paper, scissors...").toLowerCase();
 
     return userChoice;
 }
 
 const humanChoice = getHumanChoice();
 console.log(humanChoice);
+
+
+let computerScore = 0;
+let humanScore = 0;
+
+
+
+function playRound(humanChoice, ComputerChoice) {
+
+}
