2b00ca9c9114ce317335f693c1898a1dab66780d
FalehHassan <313398088+FalehHassan@users.noreply.github.com>
2026-09-17 15:52:36 +0200

    Get random choice from computer
    A number between 1 - 3 is generatede than is assigned, 1-rock 2-paper 3-scissors.

---
+++ index.js
@@ -0,0 +1,27 @@
+
+// Function getComputerChoice
+
+/* This function will randomly return rock, paper or scissors  */
+
+
+function getComputerChoice() {
+    const randomNum = Math.floor(Math.random()*3) + 1; 
+
+    let choice;
+
+    if (randomNum == 1) {
+        choice = "rock";
+    }
+    else if (randomNum == 2) {
+        choice = "paper";
+    }
+    else {
+        choice = "scissors";
+    }
+
+    return choice;
+}
+
+const ComputerChoice = getComputerChoice();
+console.log(ComputerChoice);
+
