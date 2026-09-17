bc6b347e8f540304a09fa9b4abb0cdb53c9c7dfb
FalehHassan <313398088+FalehHassan@users.noreply.github.com>
2026-09-17 17:40:54 +0200

    Compare user input to computer input with switch
    With Switch-statements we Compare userinput to the computer guess and determine the round winner by the game rules.
    round winners score variabel is incremented with 1 point for a win.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 // Function getComputerChoice
 
-// This function will randomly return rock, paper or scissors 
-
+/* This function will randomly return rock, paper or scissors 
+  
+*/
 function getComputerChoice() {
     const randomNum = Math.floor(Math.random()*3) + 1; 
 
@@ -21,8 +22,8 @@ function getComputerChoice() {
     return choice;
 }
 
-const ComputerChoice = getComputerChoice();
-console.log(ComputerChoice);
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
 
 
 // This function will prompt the user for their input
@@ -35,15 +36,53 @@ function getHumanChoice() {
     return userChoice;
 }
 
-const humanChoice = getHumanChoice();
-console.log(humanChoice);
+const humanSelection = getHumanChoice();
+console.log(humanSelection);
 
 
+// declaring two variabel to keep track of playes score.
 let computerScore = 0;
 let humanScore = 0;
 
 
 
-function playRound(humanChoice, ComputerChoice) {
+function playRound(humanChoice, computerChoice) {
 
+    switch(true) {
+         case (humanChoice === "rock" && computerChoice === "paper"):
+            console.log("You lose! Paper beats Rock");
+            computerScore++;
+            break;
+        case (humanChoice=== "rock" && computerChoice === "scissors"):
+            console.log("You win! Rock beats Scissors");
+            humanScore++;
+            break;
+
+        
+        case (humanChoice === "paper" && computerChoice === "scissors"):
+            console.log("You lose! Scissors beats Paper");
+            computerScore++;
+            break;
+        case (humanChoice === "paper" && computerChoice === "rock"):
+            console.log("You win! Paper beats Rock");
+            humanScore++;
+            break;
+
+        
+        case (humanChoice === "scissors" && computerChoice === "rock"):
+            console.log("You lose! Rock beats Scissors");
+            computerScore++;
+            break;
+        case (humanChoice=== "scissors" && computerChoice === "paper"):
+            console.log("You win! Scissors beats Paper");
+            humanScore++;
+            break;
+            
+        default:
+            console.log("It is a tie round!");
+    }
+    
 }
+
+
+playRound(humanSelection, computerSelection);
